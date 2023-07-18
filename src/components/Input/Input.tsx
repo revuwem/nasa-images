b66c33fff7a2eefeafa6c42a199877d16fa45e1e
Karina Grishaeva <revuwem@gmail.com>
2023-07-18 21:49:39 +0400

    feat(components): create Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+type Props = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input: React.FC<Props> = (props) => {
+  return (
+    <input
+      className={clsx(
+        "d-inline-block w-full pl-4 pt-3 pr-3 pb-2.5",
+        "text-grey-light",
+        "bg-black border border-grey-dark rounded",
+        "transition",
+        "focus:outline-none focus:border-grey-light"
+      )}
+      {...props}
+    />
+  );
+};
+
+export default Input;
