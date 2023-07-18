0407107fa006ae5fca50ffd9723107ca6c0d1591
Karina Grishaeva <revuwem@gmail.com>
2023-07-18 21:49:53 +0400

    feat(components): create Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+};
+
+const Button: React.FC<Props> = ({ children, ...props }) => {
+  return (
+    <button
+      className={clsx(
+        "d-inline-block w-full pt-3 pb-2.5 mt-2",
+        "text-center text-black font-bold",
+        "bg-grey-light rounded",
+        "transition",
+        "hover:bg-grey-dark",
+        "focus:outline-none focus:bg-grey-dark"
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
