08bd9c1c9a22347fc11c59b3281ceb76a65230df
Karina Grishaeva <revuwem@gmail.com>
2023-07-19 12:37:40 +0400

    feat(pages/Search): extract main tag

--- src/pages/Search/page.tsx
+++ src/pages/Search/page.tsx
@@ -3,10 +3,10 @@ import SearchResult from "@/pages/Search/components/SearchResult";
 
 const Search = () => {
   return (
-    <main className="grid grid-cols-5 gap-x-6 md:grid-cols-6">
+    <>
       <SearchForm />
       <SearchResult />
-    </main>
+    </>
   );
 };
 
