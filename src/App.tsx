62e7130142cce483d3e7442be6e7655fd411c833
Karina Grishaeva <revuwem@gmail.com>
2023-07-19 14:23:30 +0400

    feat(App): config router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Search, Collection } from "@/pages";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Search />,
+  },
+  {
+    path: "/collection/:id",
+    element: <Collection />,
+  },
+]);
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="bg-black min-h-screen py-12 px-2 md:px-12">
+      <main className="grid grid-cols-5 gap-x-6 md:grid-cols-6">
+        <RouterProvider router={router} />
+      </main>
+    </div>
+  );
 }
 
 export default App;
