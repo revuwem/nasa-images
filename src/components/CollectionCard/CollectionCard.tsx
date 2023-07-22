6230158c4b36aeffaa152cf3d1dbff65331da641
Karina Grishaeva <revuwem@gmail.com>
2023-07-23 00:15:36 +0400

    fix(components/CollectionCard): improve UI

--- src/components/CollectionCard/CollectionCard.tsx
+++ src/components/CollectionCard/CollectionCard.tsx
@@ -10,15 +10,19 @@ type Props = {
 const CollectionCard: React.FC<Props> = (props) => {
   const { preview, title, location, author } = props.data;
   return (
-    <div>
-      <div className="h-64 flex mb-3">
-        <img src={preview} alt="" className="w-full h-full object-cover" />
+    <div className="group">
+      <div className="h-72 flex mb-3 overflow-hidden">
+        <img
+          src={preview}
+          alt=""
+          className="w-full h-full object-cover transition-all duration-500 scale-110 group-hover:scale-100"
+        />
       </div>
-      <h3 className="text-xl text-grey-light font-bold mb-4">{title}</h3>
-      <div className="flex justify-between">
-        <p className="text-grey-dark">{location}</p>
-        <p className="text-grey-dark">{author}</p>
-      </div>
+      <h3 className="text-xl text-grey-light font-bold mb-4 transition group-hover:text-blue-500">
+        {title}
+      </h3>
+      {author && <p className="text-grey-dark mb-2">{author}</p>}
+      {location && <p className="text-grey-dark italic">{location}</p>}
     </div>
   );
 };
